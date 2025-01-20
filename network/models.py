8f8caeb193a2e6001a97047473c7177d7ff6168d
KK <kg.ramushu@gmail.com>
2025-01-20 22:44:31 +0200

    Models added

--- network/models.py
+++ network/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    content = models.CharField(max_length=800)
+    timestamp = models.CharField(max_length=20, null=True, blank=True)
+    def __str__(self):
+        return f"{self.poster}, {self.content}, {self.timestamp}"
+    
+class Likes(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
+    liker = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liker")
+    def __str__ (self):
+        return f"{self.post}, {self.liker}"
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    def __str__ (self):
+        return f"{self.follower}, {self.following}"
